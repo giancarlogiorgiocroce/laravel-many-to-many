1d327f4e7039e98659fbf5688d0c07e03837b83e
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-07-11 15:26:56 +0200

    Model + Seeder + Migration di Tags

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = ['One Shot', 'Short Adventure', 'One to Twenty'];
+
+        foreach ($data as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($tag);
+            $new_tag->save();
+        }
+    }
+}
